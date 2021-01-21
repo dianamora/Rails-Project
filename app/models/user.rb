7231f874dea6e7d3fd40b531e66c5f2e76f0e32f
Diana Mora <morra.dianna@gmail.com>
2021-01-21 14:07:17 -0500

    added model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :comments
+    has_many :commented_reviews, through: :comments,
+        source: :review
+    #we need to set to source this because 'commented_reviews' will look for this in the comment 
+    #model but won't find it, raising an error
+
+    # has_many :categories, through: :reviews
+
 end
