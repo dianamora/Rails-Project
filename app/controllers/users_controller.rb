4e98bc27c59973e54c7253fc345de4ba890daa38
Diana Mora <morra.dianna@gmail.com>
2021-01-21 16:02:17 -0500

    created signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
     end
 
-    def create
+    def create #if info saves successfully, log my user in and redirect to show page
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            render :new
+        end
     end
 
     def show
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
