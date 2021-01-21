38639abcf02e3d29c105ea8c86f5c72935345abc
Diana Mora <morra.dianna@gmail.com>
2021-01-21 14:47:46 -0500

    User sigup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :comments
-    has_many :commented_reviews, through: :comments,
-        source: :review
+    has_many :commented_reviews, through: :comments, source: :review
     #we need to set to source this because 'commented_reviews' will look for this in the comment 
     #model but won't find it, raising an error
+    has_secure_password #authenticate, validates pw
 
     # has_many :categories, through: :reviews
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :categories
+  #custom routes go first, because if not /:id will get read first and raise an error
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   resources :comments
   resources :users
   resources :reviews
+  # resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
